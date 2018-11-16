65178a1ff9a8d1b9a679c5d84c50af79d9a964b1
vandosant <sjskender@gmail.com>
2018-11-16 07:30:54 -0700

    Styling blog

--- src/templates/blogTemplates.js
+++ src/templates/blogTemplates.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import styles from '../blog.module.css'
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -7,7 +8,7 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post-container">
+    <div className={styles.blogPostContainer}>
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
