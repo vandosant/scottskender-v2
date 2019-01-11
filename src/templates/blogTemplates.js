a4fb1415424fc1acbe45b77b001eee82f5660e28
vandosant <sjskender@gmail.com>
2019-01-11 07:42:43 -0700

    Fix broken path

--- src/templates/blogTemplates.js
+++ src/templates/blogTemplates.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import styles from '../blog.module.css'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
